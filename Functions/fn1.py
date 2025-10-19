6586165f7326c537e1f0712b4e224396580321f4
Gani25 <memonabdulgani3d@gmail.com>
2025-10-19 14:32:49 +0530

    Create fn1.py

---
+++ Functions/fn1.py
@@ -0,0 +1,50 @@
+# WAP to accept 2 number and print addition of those 2 numbers
+# Perform same thing 5 times
+
+# Function Declaration
+def addition():
+    n1 = int(input("Enter number 1: "))
+    n2 = int(input("Enter number 2: "))
+
+    result = n1 + n2 
+
+    print(f"Addition of {n1}, {n2} = {result}")
+
+# Function Call
+addition()
+addition()
+addition()
+addition()
+addition()
+
+
+
+'''
+n1 = int(input("Enter number 1: "))
+n2 = int(input("Enter number 2: "))
+
+result = n1 + n2 
+
+print(f"Addition of {n1}, {n2} = {result}")
+
+n1 = int(input("Enter number 1: "))
+n2 = int(input("Enter number 2: "))
+
+result = n1 + n2 
+
+print(f"Addition of {n1}, {n2} = {result}")
+
+n1 = int(input("Enter number 1: "))
+n2 = int(input("Enter number 2: "))
+
+result = n1 + n2 
+
+print(f"Addition of {n1}, {n2} = {result}")
+
+n1 = int(input("Enter number 1: "))
+n2 = int(input("Enter number 2: "))
+
+result = n1 + n2 
+
+print(f"Addition of {n1}, {n2} = {result}")
+'''
